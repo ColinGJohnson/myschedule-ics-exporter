3b695d546aeaaae0056ba38fb2dc118ff330ed88
Colin Johnson <colin@cgj.dev>
2025-06-06 23:52:43 -0700

    Add calendar icon

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,5 +2,17 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: [
+      '@wxt-dev/module-react',
+      '@wxt-dev/auto-icons',
+  ],
+  autoIcons: {
+    grayscaleOnDevelopment: false,
+    baseIconPath: "assets/calendar-plus.svg"
+  },
+  manifest: {
+    action: {
+      default_title: 'MySchedule ICS Downloader',
+    },
+  },
 });
