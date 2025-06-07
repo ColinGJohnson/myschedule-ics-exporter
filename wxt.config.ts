44bcce0b03b654cfcac655af8a08c9dfd74254c4
Colin Johnson <colin@cgj.dev>
2025-06-07 00:23:01 -0700

    Add tailwind CSS and shadcn/ui

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'wxt';
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  vite: () => ({
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./"),
+      },
+    },
+  }),
   modules: [
       '@wxt-dev/module-react',
       '@wxt-dev/auto-icons',
